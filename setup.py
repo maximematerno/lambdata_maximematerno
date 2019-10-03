57fd25968c68aae4c3a5d2dbc2d84940a9fdf4d3
maxime.materno@gmail.com <maxime.materno@gmail.com>
2019-10-03 01:17:03 -0700

    modif

--- setup.py
+++ setup.py
@@ -14,13 +14,13 @@ with open("README.md", "r") as fh:
     LONG_DESCRIPTION = fh.read()
 
 setuptools.setup(
-    name="lambdata-maximematerno",
+    name="lambdata_maximematerno",
     version="0.0.2",
     author="maximematerno",
     description="A Collection of Data Science helper functions",
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
-    url="https://github.com/maximematerno/lambdata-maximematerno",
+    url="https://github.com/maximematerno/lambdata_maximematerno1",
     packages=setuptools.find_packages(),
     python_requires=">=3.5",
     install_required=REQUIRED,
